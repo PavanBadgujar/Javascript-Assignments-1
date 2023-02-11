eaf227dd77aa79863917fc11caaf4c6508d9c28d
Pavan Badgujar <112711947+PavanBadgujar@users.noreply.github.com>
2023-02-11 06:56:22 +0530

    Assignments

---
+++ 1 Square.js
@@ -0,0 +1,16 @@
+/* Q.1] Take values of length & Breadth of a Rectangle from user and check if it's a Square. */
+
+let length = prompt("Enter the length : ");
+length = Number.parseInt(length);
+
+let breadth = prompt("Enter the breadth : ");
+breadth = Number.parseInt(breadth);
+
+if (length==breadth)
+{
+    console.log("It is a Square");
+}
+else
+{
+    console.log("It is not a Square");
+}
